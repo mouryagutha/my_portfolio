78dbc9a7c5da2e35f36e1367b4d7fcc8b13d4422
java_code <kmouryap@gmail.com>
2024-06-27 01:08:45 +0530

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GitProfile from './components/gitprofile.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GitProfile config={CONFIG} />
+    <Analytics />
   </React.StrictMode>,
 );
